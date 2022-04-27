47d4338b05d59dc649265f6b4231fe5fa6055584
Raul <raulvillajr95@gmail.com>
2022-04-26 19:09:47 -0700

    Add some declare id's to start adding increments/decrements

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let sessionId = document.getElementById('session-length')
+let sessionLenNum = sessionId.innerText
+
+let breakId = document.getElementById('break-length')
+let breakLenNum = breakId.innerText
+
+console.log(breakLenNum)
