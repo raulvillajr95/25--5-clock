5988a57f9e88a4cf3273cec1c1bbe22790e8d154
Raul <raulvillajr95@gmail.com>
2022-04-27 13:40:11 -0700

    Work on Increment and Decrement functions

--- scripts.js
+++ scripts.js
@@ -1,7 +1,30 @@
-let sessionId = document.getElementById('session-length')
-let sessionLenNum = sessionId.innerText
+let breakLen = 5
+let sessionLen = 25
 
 let breakId = document.getElementById('break-length')
 let breakLenNum = breakId.innerText
+function decrementBrk() {
+  breakLen -= 1
+  breakLenNum = breakLen
+  console.log(breakLen)
+}
+function incrementBrk() {
+  element.innerText = parseInt(element.innerText) - 1
+  return element.innerText
+}
 
-console.log(breakLenNum)
+let sessionId = document.getElementById('session-length')
+let sessionLenNum = sessionId.innerText
+function decrementSess() {
+  element.innerText = parseInt(element.innerText) + 1
+  return element.innerText
+}
+function incrementSess() {
+  element.innerText = parseInt(element.innerText) - 1
+  return element.innerText
+}
+
+function reset() {
+  breakLenNum = '5'
+  sessionLenNum = '25'
+}
