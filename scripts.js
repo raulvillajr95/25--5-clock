f01eb4ee63caab81c348505532a2b4638d1b4037
Raul <raulvillajr95@gmail.com>
2022-04-27 17:54:04 -0700

    Restrict increments to 60 and decrements to 0

--- scripts.js
+++ scripts.js
@@ -3,19 +3,29 @@ let sess = 25
 
 let breakId = document.getElementById('break-length')
 let sessionId = document.getElementById('session-length')
+let timeLe = document.getElementById('time-left')
 
 function decrement(element) {
-  if (parseInt(element.innerText)) {
+  if (parseInt(element.innerText) > 0 && element == sessionId) {
+    element.textContent = parseInt(element.innerText) - 1
+    timeLe.textContent = `${element.textContent}:00`
+  } else if (parseInt(element.innerText) > 0) {
     element.textContent = parseInt(element.innerText) - 1
   }
 }
 function increment(element) {
-  if (parseInt(element.innerText)) {
+  if (parseInt(element.innerText) !== 60 && element == sessionId) {
+    element.textContent = parseInt(element.innerText) + 1
+    timeLe.textContent = `${element.textContent}:00`
+  } else if (parseInt(element.innerText) !== 60) {
     element.textContent = parseInt(element.innerText) + 1
   }
 }
 
+
+
 function resetSesh() {
   breakId.textContent = 5
   sessionId.textContent = 25
+  timeLe.textContent = '25:00'
 }
