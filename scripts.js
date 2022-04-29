583f60e1596484dde2c141c9821f1bd0cde77cca
Raul <raulvillajr95@gmail.com>
2022-04-28 21:12:52 -0700

    Go through user stories to make sure the tests are passing

--- scripts.js
+++ scripts.js
@@ -5,44 +5,91 @@ let breakId = document.getElementById('break-length')
 let sessionId = document.getElementById('session-length')
 let timeLe = document.getElementById('time-left')
 
-function decrement(element) {
-  if (parseInt(element.innerText) > 0 && element == sessionId) {
-    element.textContent = parseInt(element.innerText) - 1
-    timeLe.textContent = `${element.textContent}:00`
-  } else if (parseInt(element.innerText) > 0) {
-    element.textContent = parseInt(element.innerText) - 1
+breakId.innerText = brek
+sessionId.innerText = sess
+
+// Break Length
+function decrementBr() {
+  if (brek > 1) {
+    brek -= 1
+    breakId.textContent = brek
   }
 }
-function increment(element) {
-  if (parseInt(element.innerText) !== 60 && element == sessionId) {
-    element.textContent = parseInt(element.innerText) + 1
-    timeLe.textContent = `${element.textContent}:00`
-  } else if (parseInt(element.innerText) !== 60) {
-    element.textContent = parseInt(element.innerText) + 1
+function incrementBr() {
+  if (brek !== 60) {
+    brek += 1
+    breakId.textContent = brek
+  }
+}
+// Session Length
+function decrementSs() {
+  if (sess > 1) {
+    sess -= 1
+    sessionId.textContent = sess
+    timeLe.textContent = `${sess}:00`
+  }
+}
+function incrementSs() {
+  if (sess !== 60) {
+    sess += 1
+    sessionId.textContent = sess
+    timeLe.textContent = `${sess}:00`
   }
 }
 
-let timeCon = timeLe.textContent
-let secs = timeCon.slice(timeCon.indexOf(':') + 1)
-let mins = timeCon.slice(0, timeCon.indexOf(':'))
+let secs = 0
+// Use 'sess' for minutes
 
-let ex = 0
-function idk() {
-  if (ex == 0) {
-    console
-    ex = 59
-    timeLe.textContent = `${secs}:${ex}`
-  } else if (ex < 10) {
-    timeLe.textContent = `${secs}:0${ex}`
+/*
+within session,
+if session == '00:00',
+  sound alarm
+  run specified break
+
+within break,
+if break == '00:00'
+  sound alarm
+  run specified session
+*/
+
+function session() {
+  if (secs == 0) {
+    sess -= 1
+    secs = 59
+    timeLe.textContent = `${sess}:${secs}`
+  } else if (secs < 11) {
+    secs -= 1
+    timeLe.textContent = `${sess}:0${secs}`
   } else {
-    ex -= 1
-    timeLe.textContent = `${secs}:${ex}`
+    secs -= 1
+    timeLe.textContent = `${sess}:${secs}`
   }
 }
-setInterval(idk, 1000)
+function brekLen() {
+  if (secs == 0) {
+    sess -= 1
+    secs = 59
+    timeLe.textContent = `${sess}:${secs}`
+  } else if (secs < 11) {
+    secs -= 1
+    timeLe.textContent = `${sess}:0${secs}`
+  } else {
+    secs -= 1
+    timeLe.textContent = `${sess}:${secs}`
+  }
+}
+
+function start() {
+  
+}
+//setInterval(session, 1000)
 
 function resetSesh() {
   breakId.textContent = 5
   sessionId.textContent = 25
   timeLe.textContent = '25:00'
+  brek = 5
+  sess = 25
+  secs = 0
+  //add something to stop any running timer
 }
